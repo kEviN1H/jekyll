dc02a57856dfede7a9dd5db734ceb1e2b96bc22a
kEviN1H <kEviN1H@users.noreply.github.com>
2017-05-01 12:13:27 +0800

    Add files via upload

---
+++ _posts/gcc2-4.8.sh
@@ -0,0 +1,9 @@
+cd /usr/bin
+rm c++ && ln -s c++-4.8 c++
+rm cc  && ln -s gcc-4.8 cc
+rm gcc && ln -s gcc-4.8 gcc 
+rm gcc-ar && ln -s gcc-ar-4.8 gcc-ar
+rm gcc-nm && ln -s gcc-nm-4.8 gcc-nm
+rm gcc-ranlib && ln -s gcc-ranlib-4.8 gcc-ranlib
+
+
