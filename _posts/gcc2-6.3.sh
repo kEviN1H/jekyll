83dbe0b9fe896b84090606cec816729c2d113f17
kEviN1H <kEviN1H@users.noreply.github.com>
2017-05-01 12:14:35 +0800

    Add files via upload

---
+++ _posts/gcc2-6.3.sh
@@ -0,0 +1,9 @@
+cd /usr/bin
+rm c++ && ln -s c++-6.3 c++
+rm cc  && ln -s gcc-6.3 cc
+rm gcc && ln -s gcc-6.3 gcc 
+rm gcc-ar && ln -s gcc-ar-6.3 gcc-ar
+rm gcc-nm && ln -s gcc-nm-6.3 gcc-nm
+rm gcc-ranlib && ln -s gcc-ranlib-6.3 gcc-ranlib
+
+
